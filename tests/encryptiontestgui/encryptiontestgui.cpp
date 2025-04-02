f8544d8d141667cef36f44373227233d84130ba9
Andro Ranogajec <ranogaet@gmail.com>
2025-04-02 12:28:34 +0300

    Add Dialog i.e. modal window, reject, accept buttons

--- tests/encryptiontestgui/encryptiontestgui.cpp
+++ tests/encryptiontestgui/encryptiontestgui.cpp
@@ -8,7 +8,10 @@
 #include "encryptiontestgui.h"
 #include "encryption/encryptionutils.h"
 #include <QApplication>
+#include <QDialog>
+#include <QDialogButtonBox>
 #include <QLabel>
+#include <QLineEdit>
 #include <QPushButton>
 #include <QTextEdit>
 #include <QVBoxLayout>
@@ -27,14 +30,33 @@ EncryptionTestGui::EncryptionTestGui(QWidget *parent)
   auto pushButtonDeriveMasterKey =
       new QPushButton(QStringLiteral("Derive Master Key"), this);
   mainLayout->addWidget(pushButtonDeriveMasterKey);
+
   connect(pushButtonDeriveMasterKey, &QPushButton::clicked, this, [this]() {
-    password = QStringLiteral("admin");
-    userId = QStringLiteral("root");
-    mMasterKey = EncryptionUtils::deriveKey(userId.toUtf8(), password.toUtf8(),
-                                            1000, 32);
-    qDebug() << "Derived Master Key:" << mMasterKey.toHex();
-    mTextEditResult->setPlainText(
-        QStringLiteral("Master Key derived successfully!"));
+    QDialog *dialog = new QDialog(this);
+    dialog->setWindowTitle(QStringLiteral("Credentials"));
+
+    QLineEdit *userIdEdit = new QLineEdit;
+    QLineEdit *passwordEdit = new QLineEdit;
+    passwordEdit->setEchoMode(QLineEdit::Password);
+
+    QGridLayout *layout = new QGridLayout(dialog);
+    layout->addWidget(new QLabel(QStringLiteral("UserId: "), dialog), 0, 0);
+    layout->addWidget(userIdEdit, 0, 1);
+    layout->addWidget(new QLabel(QStringLiteral("Password: "), dialog), 1, 0);
+    layout->addWidget(passwordEdit, 1, 1);
+
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(
+        QDialogButtonBox::Ok | QDialogButtonBox::Cancel, dialog);
+    layout->addWidget(buttonBox, 2, 0, 1, 2);
+    // dialog->show();
+    dialog->setLayout(layout);
+
+    connect(buttonBox, &QDialogButtonBox::accepted, dialog, &QDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, dialog, &QDialog::reject);
+
+    if (dialog->exec()) {
+      // test
+    }
   });
 
   auto pushButtonGenerateRSAKey =
