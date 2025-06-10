4946ef08cc5fea303d954d572e557ef7bf138dc5
Andro Ranogajec <ranogaet@gmail.com>
2025-06-10 17:02:14 +0300

    Implement autotest functions

--- src/core/autotests/rsapairtest.cpp
+++ src/core/autotests/rsapairtest.cpp
@@ -16,6 +16,15 @@ RsaPairTest::RsaPairTest(QObject *parent)
 
 void RsaPairTest::rsaPairGenerationNonDeterminismTest()
 {
+    EncryptionUtils::RSAKeyPair rsaPair1;
+    EncryptionUtils::RSAKeyPair rsaPair2;
+
+    for (int i = 0; i <= 10; i++) {
+        rsaPair1 = EncryptionUtils::generateRSAKey();
+        rsaPair2 = EncryptionUtils::generateRSAKey();
+        QVERIFY(rsaPair1.publicKey != rsaPair2.publicKey);
+        QVERIFY(rsaPair1.privateKey != rsaPair2.privateKey);
+    }
 }
 
 /**
@@ -41,6 +50,37 @@ void RsaPairTest::rsaPairGenerationNonDeterminismTest()
  */
 void RsaPairTest::encodeDecodeDeterminismTest()
 {
+    EncryptionUtils::RSAKeyPair rsaPair1;
+    EncryptionUtils::RSAKeyPair rsaPair2;
+
+    QByteArray privateKey1;
+    QByteArray privateKey2;
+
+    QByteArray masterKey;
+
+    QByteArray encodedPrivateKey1;
+    QByteArray encodedPrivateKey2;
+    QByteArray decodedPrivateKey1;
+    QByteArray decodedPrivateKey2;
+
+    for (int i = 0; i <= 10; i++) {
+        rsaPair1 = EncryptionUtils::generateRSAKey();
+        rsaPair2 = EncryptionUtils::generateRSAKey();
+
+        privateKey1 = rsaPair1.privateKey;
+        privateKey2 = rsaPair2.privateKey;
+
+        masterKey = EncryptionUtils::getMasterKey(EncryptionUtils::generateRandomText(32), EncryptionUtils::generateRandomText(32));
+
+        encodedPrivateKey1 = EncryptionUtils::encodePrivateKey(rsaPair1.privateKey, masterKey);
+        encodedPrivateKey2 = EncryptionUtils::encodePrivateKey(rsaPair2.privateKey, masterKey);
+
+        decodedPrivateKey1 = EncryptionUtils::decodePrivateKey(encodedPrivateKey1, masterKey);
+        decodedPrivateKey2 = EncryptionUtils::decodePrivateKey(encodedPrivateKey2, masterKey);
+
+        QVERIFY(decodedPrivateKey1 == privateKey1);
+        QVERIFY(decodedPrivateKey2 == privateKey2);
+    }
 }
 
 #include "moc_rsapairtest.cpp"
